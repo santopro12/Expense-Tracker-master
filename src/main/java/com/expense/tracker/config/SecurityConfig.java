e227981e7a8a0abfee6cfc76ffdce5e039588308
Santosh <prajapatis1100@gmail.com>
2026-07-06 02:13:35 -0400

    commit

--- src/main/java/com/expense/tracker/config/SecurityConfig.java
+++ src/main/java/com/expense/tracker/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.expense.tracker.config;
+  package com.expense.tracker.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
